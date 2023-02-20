2bcd730d74ed4362fd09863eba18ea65c45139ff
twkim3 <twkim@b4play.io>
2023-02-21 00:39:47 +0900

    웨않되

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 function sum(a:number, b:   number) {
   return a + b;
 }
-function   mul(a:number, b  :number) {
- 
-   
+ function   mul(a:number, b  :number) {
+  
+     
 
      return      a    *   b
 }
