738c138dad7961e52015b0cbf45aa7508bdcdab7
twkim3 <twkim@b4play.io>
2023-02-26 10:28:25 +0900

    groupt test 수정(tobe -> toStrickEqual)

--- src/sub.test.ts
+++ src/sub.test.ts
@@ -1,7 +1,7 @@
 import { group } from "./main";
 
 test("groupBy 실행", () => {
-  expect(group([1, 2, 3, 4, 5, 6], (n) => (n % 2 === 0 ? "짝수" : "홀수"))).toBe({
+  expect(group([1, 2, 3, 4, 5, 6], (n) => (n % 2 === 0 ? "짝수" : "홀수"))).toStrictEqual({
     홀수: [1, 3, 5],
     짝수: [2, 4, 6],
   });
